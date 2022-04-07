5793837c212359e8ca378d17f7491721b98d76b6
beb-commits <100702756+beb-commits@users.noreply.github.com>
2022-04-07 17:32:58 +0100

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,39 @@
+function oneless() {
+	div = document.getElementById('poem');
+	for (var i = 99; i > 1; i--) {
+		div.innerHTML +=(i + " bottles of beer on the wall,");
+		div.innerHTML +=("<br>");
+		div.innerHTML +=(i + " bottles of beer");
+		div.innerHTML +=("<br>");
+		div.innerHTML +=("Take one down and pass it around,");
+		div.innerHTML +=("<br>");
+		div.innerHTML +=((i - 1) + " bottles of beer on the wall");
+		div.innerHTML +=("<br>");
+		div.innerHTML +=("<br>");
+}
+		
+			div.innerHTML +=("One bottle of beer on the wall");
+			div.innerHTML +=("<br>");
+			div.innerHTML +=("One bottle of beer");
+			div.innerHTML +=("<br>");
+			div.innerHTML +=("Take one down and pass it around,");
+			div.innerHTML +=("<br>");
+			div.innerHTML +=("no more bottles of beer on the wall");
+			div.innerHTML +=("<br>");
+			div.innerHTML +=("<br>");
+		
+			div.innerHTML +=("No more bottles of beer on the wall,");
+			div.innerHTML +=("<br>");
+			div.innerHTML +=("no more bottles of beer");
+			div.innerHTML +=("<br>");
+			div.innerHTML +=("Go to the store and buy some more,");
+			div.innerHTML +=("<br>");
+			div.innerHTML +=("99 bottles of beer on the wall");
+			div.innerHTML +=("<br>");
+			div.innerHTML +=("<br>");
+		
+	}
+
+
+
+
